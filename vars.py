d491278d5c30f9398bef324b8218da7da9469fba
princerathaur1302 <princerathaur1302@gmail.com>
2025-08-08 16:27:04 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,9 +7,10 @@ OWNER = int(environ.get("OWNER", "6775793050"))
 CREDIT = "[♛彡 T͙E͙A͙M͙ L͙B͙ 彡♛](https://t.me/contact_262524_bot)"
 YT = "[𓊈𒆜Y͎O͎U͎T͎U͎B͎E͎𒆜𓊉](https://youtube.com/@LocalBoyPrince)"
 ALL = "[♛A͓̽L͓̽L͓̽ B͓̽A͓̽T͓̽C͓̽H͓̽♛](https://t.me/addlist/Yfez5bB2FiljMzE1)"
-AUTH_USER = os.environ.get('AUTH_USERS', '6775793050').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7281824001').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
 
