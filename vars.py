bbf97841c880578239034f664e4ccb673bac7c49
princerathaur1302 <princerathaur1302@gmail.com>
2025-08-08 13:08:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,8 +4,11 @@ API_ID = int(environ.get("API_ID", "20214595"))
 API_HASH = environ.get("API_HASH", "4763f66ce1a18c2dd491a5048891926c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "6775793050"))
-CREDIT = "[꧁.💕Ŧ€ΔΜ Łβ💖.꧂](https://t.me/contact_262524_bot)"
+CREDIT = "[♛彡 T͙E͙A͙M͙ L͙B͙ 彡♛](https://t.me/contact_262524_bot)"
+YT = "[𝚈̷𝙾̷𝚄̷𝚃̷𝚄̷𝙱̷𝙴̷](https://youtube.com/@LocalBoyPrince)"
+ALL = "[♛A͓̽L͓̽L͓̽ B͓̽A͓̽T͓̽C͓̽H͓̽♛](https://t.me/addlist/Yfez5bB2FiljMzE1)"
 AUTH_USER = os.environ.get('AUTH_USERS', '6775793050').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
